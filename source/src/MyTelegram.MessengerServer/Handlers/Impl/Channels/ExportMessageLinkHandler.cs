0805be3d9d1b5dd44b0c93c43f3f41f4db2a2beb
loyldg <MyTelegramServer@outlook.com>
2022-07-27 23:11:21 +0800

    Feat:Returns empty data for ExportMessageLinkHandler

--- source/src/MyTelegram.MessengerServer/Handlers/Impl/Channels/ExportMessageLinkHandler.cs
+++ source/src/MyTelegram.MessengerServer/Handlers/Impl/Channels/ExportMessageLinkHandler.cs
@@ -4,11 +4,23 @@ using MyTelegram.Schema.Channels;
 namespace MyTelegram.MessengerServer.Handlers.Impl.Channels;
 
 public class ExportMessageLinkHandler : RpcResultObjectHandler<RequestExportMessageLink, IExportedMessageLink>,
-    IExportMessageLinkHandler
+    IExportMessageLinkHandler, IProcessedHandler
 {
+    private readonly IPeerHelper _peerHelper;
+
+    public ExportMessageLinkHandler(IPeerHelper peerHelper)
+    {
+        _peerHelper = peerHelper;
+    }
+
     protected override Task<IExportedMessageLink> HandleCoreAsync(IRequestInput input,
         RequestExportMessageLink obj)
     {
-        throw new NotImplementedException();
+        var peer = _peerHelper.GetChannel(obj.Channel);
+        return Task.FromResult<IExportedMessageLink>(new TExportedMessageLink
+        {
+            Link = $"Not support export link.Id={obj.Id},channelId={peer.PeerId}",
+            Html = "No html"
+        });
     }
 }
