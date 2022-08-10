f13d95a9dbc1a5bc3475664a2ba74bca679e3b01
loyldg <MyTelegramServer@outlook.com>
2022-08-10 11:30:15 +0800

    Feat:Impl GetDialogFiltersHandler

--- source/src/MyTelegram.MessengerServer/Handlers/Impl/Messages/GetDialogFiltersHandler.cs
+++ source/src/MyTelegram.MessengerServer/Handlers/Impl/Messages/GetDialogFiltersHandler.cs
@@ -6,11 +6,28 @@ namespace MyTelegram.MessengerServer.Handlers.Impl.Messages;
 public class GetDialogFiltersHandler : RpcResultObjectHandler<RequestGetDialogFilters, TVector<IDialogFilter>>,
     IGetDialogFiltersHandler, IProcessedHandler
 {
-    protected override Task<TVector<IDialogFilter>> HandleCoreAsync(IRequestInput input,
+    private readonly IQueryProcessor _queryProcessor;
+    private readonly IObjectMapper _objectMapper;
+    public GetDialogFiltersHandler(IQueryProcessor queryProcessor,
+        IObjectMapper objectMapper)
+    {
+        _queryProcessor = queryProcessor;
+        _objectMapper = objectMapper;
+    }
+
+    protected override async Task<TVector<IDialogFilter>> HandleCoreAsync(IRequestInput input,
         RequestGetDialogFilters obj)
     {
-        var filters = new TVector<IDialogFilter>();
+        var filterReadModels = await _queryProcessor.ProcessAsync(new GetDialogFiltersQuery(input.UserId), default)
+            .ConfigureAwait(false);
 
-        return Task.FromResult(filters);
+        var filters = new TVector<IDialogFilter>();
+        foreach (var filterReadModel in filterReadModels)
+        {
+            var filter = _objectMapper.Map<DialogFilter, TDialogFilter>(filterReadModel.Filter);
+            filters.Add(filter);
+        }
+
+        return filters;
     }
 }
