dd52ceb54e08e7015d4825ee5931b51b164e997c
loyldg <MyTelegramServer@outlook.com>
2022-08-22 17:14:16 +0800

    Remove unused code

--- source/src/MyTelegram.MessengerServer.GrpcService/Program.cs
+++ source/src/MyTelegram.MessengerServer.GrpcService/Program.cs
@@ -36,18 +36,4 @@ var app = builder.Build();
 app.MapGrpcService<ChatMemberLoaderGrpcService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-//app.UseRouting();
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapGrpcService<ChatMemberLoaderGrpcService>();
-//    //endpoints.MapGrpcService<ChatGrpcService>();
-
-//    endpoints.MapGet("/", async context =>
-//    {
-//        await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-//    });
-//});
-
-//app.MapGet("/", () => "Hello World!");
-
 app.Run();
