c41c3077ee8c3aeec02d0b64d85d621ba6f68da0
loyldg <MyTelegramServer@outlook.com>
2022-08-31 15:28:39 +0800

    Fix:User sign up issues,get userId from userReadModel instead of from cache

--- source/src/MyTelegram.MessengerServer/Handlers/Impl/Auth/SignUpHandler.cs
+++ source/src/MyTelegram.MessengerServer/Handlers/Impl/Auth/SignUpHandler.cs
@@ -12,30 +12,35 @@ public class SignUpHandler : RpcResultObjectHandler<RequestSignUp, IAuthorizatio
     private readonly ICacheManager<UserCacheItem> _cacheManager;
     private readonly ICommandBus _commandBus;
     private readonly IRandomHelper _randomHelper;
+    private readonly IQueryProcessor _queryProcessor;
 
     public SignUpHandler(
         ICommandBus commandBus,
         IRandomHelper randomHelper,
-        ICacheManager<UserCacheItem> cacheManager)
+        ICacheManager<UserCacheItem> cacheManager,
+        IQueryProcessor queryProcessor)
     {
         _commandBus = commandBus;
         _randomHelper = randomHelper;
         _cacheManager = cacheManager;
+        _queryProcessor = queryProcessor;
     }
 
     protected override async Task<IAuthorization> HandleCoreAsync(IRequestInput input,
         RequestSignUp obj)
     {
-        // 用户登录成功后，会将用户Id和电话号码的对应关系进行缓存，登录的时候需要先判断该用户电话是否已经注册，如果已注册就直接使用之前的Uid
-        var cachedUserItem = await _cacheManager.GetAsync(
-            UserCacheItem.GetCacheKey(obj.PhoneNumber.ToPhoneNumber())).ConfigureAwait(false);
+        var phoneNumber = obj.PhoneNumber.ToPhoneNumber();
+        var userReadModel = await _queryProcessor
+            .ProcessAsync(new GetUserByPhoneNumberQuery(phoneNumber), default)
+            .ConfigureAwait(false);
+        var userId = userReadModel?.UserId ?? 0;
 
-        var command = new CheckSignUpCodeCommand(AppCodeId.Create(obj.PhoneNumber.ToPhoneNumber(), obj.PhoneCodeHash),
+        var command = new CheckSignUpCodeCommand(AppCodeId.Create(phoneNumber, obj.PhoneCodeHash),
             input.ToRequestInfo(),
             obj.PhoneCodeHash,
-            cachedUserItem?.UserId ?? 0,
+            userId,
             _randomHelper.NextLong(),
-            obj.PhoneNumber,
+            phoneNumber,
             obj.FirstName,
             obj.LastName,
             Guid.NewGuid());
