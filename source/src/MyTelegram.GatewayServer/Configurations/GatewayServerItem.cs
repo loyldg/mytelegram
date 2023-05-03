b4e799d0be5ceb1b808ed5fbd1ae582981c692c8
loyldg <MyTelegramServer@outlook.com>
2023-05-03 22:02:39 +0800

    Fix:Compiler warnings

--- source/src/MyTelegram.GatewayServer/Configurations/GatewayServerItem.cs
+++ source/src/MyTelegram.GatewayServer/Configurations/GatewayServerItem.cs
@@ -5,7 +5,7 @@ public class GatewayServerItem
     public ServerType ServerType { get; set; }
     public bool Enabled { get; set; }
     public bool Ssl { get; set; }
-    public string Ip { get; set; }
+    public string? Ip { get; set; }
     public int Port { get; set; }
     public bool Ipv6 { get; set; }
     public string CertPemFilePath { get; set; } = string.Empty;
