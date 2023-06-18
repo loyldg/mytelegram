ea8b3ea5026de70b837e95031f0ff37681810942
mytelegram <mytelegramserver@outlook.com>
2023-06-18 18:23:40 +0800

    Remove references to MyTelegram.Messenger

--- source/src/MyTelegram.MessengerServer.GrpcService/MyTelegramMessengerServerGrpcServiceExtensions.cs
+++ source/src/MyTelegram.MessengerServer.GrpcService/MyTelegramMessengerServerGrpcServiceExtensions.cs
@@ -1,11 +1,7 @@
 ﻿using EventFlow;
 using EventFlow.Extensions;
 using EventFlow.MongoDB.Extensions;
-using MyTelegram.Core;
 using MyTelegram.Domain.EventFlow;
-using MyTelegram.MessengerServer.Extensions;
-using MyTelegram.MessengerServer.Services.Impl;
-using MyTelegram.MessengerServer.Services.Interfaces;
 using MyTelegram.QueryHandlers.MongoDB.Channel;
 using MyTelegram.QueryHandlers.MongoDB.Chat;
 using MyTelegram.ReadModel.MongoDB;
@@ -16,9 +12,6 @@ public static class MyTelegramMessengerServerGrpcServiceExtensions
 {
     private static IServiceCollection AddMyTelegramGrpcService(this IServiceCollection services)
     {
-        services.AddSingleton<IRpcResultProcessor, RpcResultProcessor>();
-
-        services.RegisterAllMappers();
         return services;
     }
 
@@ -36,12 +29,12 @@ public static class MyTelegramMessengerServerGrpcServiceExtensions
                 );
                 configure?.Invoke(options);
             })
-            .AddMyTelegramCoreServices()
-            .AddMyTelegramHandlerServices()
-            .AddMyTelegramMessengerServer()
+            //.AddMyTelegramCoreServices()
+            //.AddMyTelegramHandlerServices()
+            //.AddMyTelegramMessengerServer()
             .AddMyTelegramGrpcService()
             .AddMyEventFlow()
-            .RegisterAllMappers()
+            //.RegisterAllMappers()
             ;
     }
 }
