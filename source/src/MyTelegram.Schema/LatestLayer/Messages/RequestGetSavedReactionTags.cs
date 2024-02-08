9f5be971164b56eeb056dda26dcd6e8feba5cdd2
mytelegram <mytelegramserver@outlook.com>
2024-02-08 13:49:12 +0800

    Update API schema on layer 173

--- source/src/MyTelegram.Schema/LatestLayer/Messages/RequestGetSavedReactionTags.cs
+++ source/src/MyTelegram.Schema/LatestLayer/Messages/RequestGetSavedReactionTags.cs
@@ -6,14 +6,17 @@ namespace MyTelegram.Schema.Messages;
 ///<summary>
 /// See <a href="https://corefork.telegram.org/method/messages.getSavedReactionTags" />
 ///</summary>
-[TlObject(0x761ddacf)]
+[TlObject(0x3637e05b)]
 public sealed class RequestGetSavedReactionTags : IRequest<MyTelegram.Schema.Messages.ISavedReactionTags>
 {
-    public uint ConstructorId => 0x761ddacf;
+    public uint ConstructorId => 0x3637e05b;
+    public BitArray Flags { get; set; } = new BitArray(32);
+    public MyTelegram.Schema.IInputPeer? Peer { get; set; }
     public long Hash { get; set; }
 
     public void ComputeFlag()
     {
+        if (Peer != null) { Flags[0] = true; }
 
     }
 
@@ -21,11 +24,15 @@ public sealed class RequestGetSavedReactionTags : IRequest<MyTelegram.Schema.Mes
     {
         ComputeFlag();
         writer.Write(ConstructorId);
+        writer.Write(Flags);
+        if (Flags[0]) { writer.Write(Peer); }
         writer.Write(Hash);
     }
 
     public void Deserialize(ref SequenceReader<byte> reader)
     {
+        Flags = reader.ReadBitArray();
+        if (Flags[0]) { Peer = reader.Read<MyTelegram.Schema.IInputPeer>(); }
         Hash = reader.ReadInt64();
     }
 }
