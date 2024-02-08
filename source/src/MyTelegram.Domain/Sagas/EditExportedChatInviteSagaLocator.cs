ac672f2cd1a2c4404213f8d864bd7f3739d08915
mytelegram <mytelegramserver@outlook.com>
2024-02-08 14:57:42 +0800

    refactor: Cleanup code

--- source/src/MyTelegram.Domain/Sagas/EditExportedChatInviteSagaLocator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyTelegram.Domain.Sagas;
-
-public class EditExportedChatInviteSagaLocator : DefaultSagaLocator<EditExportedChatInviteSaga, EditExportedChatInviteSagaId>
-{
-    protected override EditExportedChatInviteSagaId CreateSagaId(string requestId)
-    {
-        return new EditExportedChatInviteSagaId(requestId);
-    }
-}
