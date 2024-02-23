1b8b811ace87886f647f3a3291cfc49f0739d8d2
mytelegram <mytelegramserver@outlook.com>
2024-02-23 17:03:39 +0800

    Update version to 0.18.223

--- build/docker/get-version.sh
+++ build/docker/get-version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#currentDate=`date +%-m%d`
+currentDate=`date +%-m%d`
 #imageVersion=0.6.$currentDate
-version=0.15.1214
+version=0.18.$currentDate
 imageVersion=$version-alpine
 echo version: $version
 echo image version: $imageVersion
