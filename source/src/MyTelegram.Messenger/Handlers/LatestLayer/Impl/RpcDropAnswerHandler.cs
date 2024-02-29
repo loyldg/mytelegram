b378e3f5293a34a4cfb53246aa8903f9809c2fc3
mytelegram <mytelegramserver@outlook.com>
2024-02-29 20:39:07 +0800

    fix: RpcDropAnswerHandler should return rpc result

--- source/src/MyTelegram.Messenger/Handlers/LatestLayer/Impl/RpcDropAnswerHandler.cs
+++ source/src/MyTelegram.Messenger/Handlers/LatestLayer/Impl/RpcDropAnswerHandler.cs
@@ -2,7 +2,7 @@
 
 namespace MyTelegram.Handlers.Impl;
 
-internal sealed class RpcDropAnswerHandler : BaseObjectHandler<RequestRpcDropAnswer, IRpcDropAnswer>,
+internal sealed class RpcDropAnswerHandler : RpcResultObjectHandler<RequestRpcDropAnswer, IRpcDropAnswer>,
     IRpcDropAnswerHandler
 {
     protected override Task<IRpcDropAnswer> HandleCoreAsync(IRequestInput input,
