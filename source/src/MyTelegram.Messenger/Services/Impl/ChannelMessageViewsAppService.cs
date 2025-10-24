04e7eb0ac38f4d0e9ef6dca4828dd5864cc84838
mytelegram <mytelegramserver@outlook.com>
2025-10-24 15:51:08 +0800

    refactor: Message views optimization
    1. Use the new SimpleCuckooFilter implementation to remove the dependency on Microsoft.Cuckoo.
    
    2. Views reset after 3 days, allowing the same user to increase the view count again for messages they’ve already viewed.
    
    3. Uniqueness is now determined by UserId + ChannelId + MessageId, instead of PermAuthKeyId + UserId + ChannelId + MessageId.

--- source/src/MyTelegram.Messenger/Services/Impl/ChannelMessageViewsAppService.cs
+++ source/src/MyTelegram.Messenger/Services/Impl/ChannelMessageViewsAppService.cs
@@ -1,5 +1,5 @@
-﻿using EventFlow.Exceptions;
-using MyTelegram.Messenger.Services.Filters;
+﻿using MyTelegram.Messenger.Services.Filters;
+using System.Buffers.Binary;
 using IMessageViews = MyTelegram.Schema.IMessageViews;
 
 namespace MyTelegram.Messenger.Services.Impl;
@@ -7,28 +7,26 @@ namespace MyTelegram.Messenger.Services.Impl;
 public class ChannelMessageViewsAppService(
     IQueryProcessor queryProcessor,
     ICommandBus commandBus,
-    ICuckooFilter cuckooFilter)
-    : IChannelMessageViewsAppService, ITransientDependency
+    ILogger<ChannelMessageViewsAppService> logger)
+    : IChannelMessageViewsAppService, ISingletonDependency
 {
-    public async Task IncrementViewsIfNotIncrementedAsync(long selfUserId,
-        long authKeyId,
-        long channelId,
-        int messageId)
-    {
-        var key = GetFilterKey(selfUserId, authKeyId, channelId, messageId);
-        var isExists = await cuckooFilter.ExistsAsync(key);
-        if (!isExists)
-        {
-            await cuckooFilter.AddAsync(key);
-        }
-    }
+    // 1M views per day, and the data from the first day will be cleared after 3 days.
+    // 1M entries take up more than 2.5 MB of memory
+    // We can increase this value based on the number of users in the system.
+    private static readonly SimpleCuckooFilter Filter1 = new();
+    private static readonly SimpleCuckooFilter Filter2 = new();
+    private static readonly SimpleCuckooFilter Filter3 = new();
 
-    private byte[] GetFilterKey(long selfUserId,
-        long authKeyId,
-        long channelId,
-        int messageId) =>
-        Encoding.UTF8.GetBytes(
-            $"{MyTelegramConsts.ChannelMessageViewsBloomFilterKey}_{selfUserId}_{authKeyId}_{channelId}_{messageId}");
+    private static readonly string ViewsFilterFileName = "views";
+    private static SimpleCuckooFilter _currentFilter = Filter1;
+    private static int _currentIndex;
+
+    public static void GenerateFilterKey(long selfUserId, long channelId, int messageId, Span<byte> key)
+    {
+        BinaryPrimitives.WriteInt64LittleEndian(key, selfUserId);
+        BinaryPrimitives.WriteInt64LittleEndian(key.Slice(8), channelId);
+        BinaryPrimitives.WriteInt32LittleEndian(key.Slice(16), messageId);
+    }
 
     public async Task<IList<IMessageViews>> GetMessageViewsAsync(long selfUserId,
         long authKeyId,
@@ -36,25 +34,31 @@ public class ChannelMessageViewsAppService(
         List<int> messageIdList)
     {
         var messageIdGreaterThanZeroList = messageIdList.Where(p => p > 0).ToList();
-        var keyList = messageIdGreaterThanZeroList
-            .Select(p => GetFilterKey(selfUserId, authKeyId, channelId, p)).ToList();
 
         var needIncrementMessageIdList = new List<int>();
-        var index = 0;
-
-        foreach (var key in keyList)
+        //Span<byte> key = stackalloc byte[20];
+        var tempBytes = ArrayPool<byte>.Shared.Rent(20);
+        var key = tempBytes.AsSpan(0, 20);
+        try
         {
-            var isExists = await cuckooFilter.ExistsAsync(key);
-            if (!isExists)
+            foreach (var id in messageIdList)
             {
-                await cuckooFilter.AddAsync(key);
-                needIncrementMessageIdList.Add(messageIdGreaterThanZeroList[index]);
+                GenerateFilterKey(selfUserId, channelId, id, key);
+                var canIncrementViews = CanIncrementViews(key);
+                if (canIncrementViews)
+                {
+                    _currentFilter.Add(key);
+                    needIncrementMessageIdList.Add(id);
+                }
             }
-            index++;
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(tempBytes);
         }
 
         var messageViews = (await queryProcessor
-                    .ProcessAsync(new GetMessageViewsQuery(channelId, messageIdGreaterThanZeroList), default)
+                    .ProcessAsync(new GetMessageViewsQuery(channelId, messageIdGreaterThanZeroList))
                     .ConfigureAwait(false))
                 .ToDictionary(k => k.MessageId, v => v)
             ;
@@ -66,9 +70,9 @@ public class ChannelMessageViewsAppService(
                 var command = new IncrementViewsCommand(MessageId.Create(channelId, messageId));
                 await commandBus.PublishAsync(command);
             }
-            catch (DomainError)
+            catch (Exception ex)
             {
-                //
+                logger.LogError(ex, "IncrementViews failed");
             }
         }
 
@@ -112,5 +116,115 @@ public class ChannelMessageViewsAppService(
         }
 
         return messageViewsToClient;
+    }
+
+    public void IncrementViews(long selfUserId, long channelId, int messageId)
+    {
+        Span<byte> key = stackalloc byte[20];
+        GenerateFilterKey(selfUserId, channelId, messageId, key);
+        if (CanIncrementViews(key))
+        {
+            _currentFilter.Add(key);
+        }
+    }
+
+    public Task IncrementViewsIfNotIncrementedAsync(long selfUserId,
+                    long authKeyId,
+        long channelId,
+        int messageId)
+    {
+        IncrementViews(selfUserId, channelId, messageId);
+
+        return Task.CompletedTask;
+    }
+
+    public void LoadViewsFilters()
+    {
+        LoadViewsFilterDataFromLocalCachedFile(1, Filter1);
+        LoadViewsFilterDataFromLocalCachedFile(2, Filter2);
+        LoadViewsFilterDataFromLocalCachedFile(3, Filter3);
+    }
+
+    public void RotateDaily()
+    {
+        var next = (_currentIndex + 1) % 3;
+        _currentIndex = next;
+        switch (next)
+        {
+            case 0:
+                Filter1.Clear();
+                _currentFilter = Filter1;
+                break;
+
+            case 1:
+                Filter2.Clear();
+                _currentFilter = Filter2;
+                break;
+
+            case 2:
+                Filter3.Clear();
+                _currentFilter = Filter3;
+                break;
+        }
+
+        logger.LogInformation("The channel views count filter has been rotated to filter {Index}", _currentIndex + 1);
+    }
+
+    public void SaveViewsFilters()
+    {
+        try
+        {
+            var viewsFileName1 = Path.Combine(AppContext.BaseDirectory, $"{ViewsFilterFileName}_1");
+            var viewsFileName2 = Path.Combine(AppContext.BaseDirectory, $"{ViewsFilterFileName}_2");
+            var viewsFileName3 = Path.Combine(AppContext.BaseDirectory, $"{ViewsFilterFileName}_3");
+
+            File.WriteAllBytes(viewsFileName1, Filter1.GetData());
+            File.WriteAllBytes(viewsFileName2, Filter2.GetData());
+            File.WriteAllBytes(viewsFileName3, Filter3.GetData());
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Save views filter failed");
+        }
+    }
+
+    private bool CanIncrementViews(Span<byte> key)
+    {
+        var isExists1 = Filter1.Contains(key);
+        var isExists2 = Filter2.Contains(key);
+        var isExists3 = Filter3.Contains(key);
+
+        var isExists = isExists1 || isExists2 || isExists3;
+
+        return !isExists;
+    }
+
+    private void LoadViewsFilterDataFromLocalCachedFile(int index, SimpleCuckooFilter filter)
+    {
+        try
+        {
+            var viewsFileName = Path.Combine(AppContext.BaseDirectory, $"{ViewsFilterFileName}_{index}");
+            if (File.Exists(viewsFileName))
+            {
+                var filterData = File.ReadAllBytes(viewsFileName);
+                var isLoaded = filter.LoadData(filterData);
+                if (isLoaded)
+                {
+                    logger.LogInformation("Channel message views filter {Index} loaded successfully, bytes: {Length}",
+                        index,
+                        filterData.Length);
+                }
+                else
+                {
+                    logger.LogWarning(
+                        "Channel message views filter {Index} data failed to load, the filter's configuration parameters may have changed.",
+                        index);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Load views filter data failed");
+        }
     }
 }
