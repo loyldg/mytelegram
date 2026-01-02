1c7d40dddf3a217271676576a52400f6806d6c83
mytelegram <mytelegramserver@outlook.com>
2026-01-02 11:06:25 +0800

    Update MyTelegram.Queries to reference MyTelegram.ReadModel.Interfaces instead of MyTelegram.ReadModel

--- source/src/MyTelegram.Queries/GlobalUsings.cs
+++ source/src/MyTelegram.Queries/GlobalUsings.cs
@@ -1,7 +1,4 @@
 ﻿global using EventFlow.Queries;
-global using MyTelegram.Domain.Aggregates.Dialog;
 global using MyTelegram.ReadModel;
-global using MyTelegram.ReadModel.Impl;
 global using MyTelegram.ReadModel.Interfaces;
-global using MyTelegram.Domain.Aggregates.PeerNotifySetting;
 global using MyTelegram.Schema;
