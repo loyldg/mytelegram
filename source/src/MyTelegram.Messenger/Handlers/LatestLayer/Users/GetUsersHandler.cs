658d8d5a88a00cb80e1305e80da79d7840913418
mytelegram <mytelegramserver@outlook.com>
2026-01-04 14:04:32 +0800

    fix: Return TVector<TUserEmpty> instead of an empty array when GetUsersHandler returns no results.

--- source/src/MyTelegram.Messenger/Handlers/LatestLayer/Users/GetUsersHandler.cs
+++ source/src/MyTelegram.Messenger/Handlers/LatestLayer/Users/GetUsersHandler.cs
@@ -1,4 +1,5 @@
 namespace MyTelegram.Messenger.Handlers.LatestLayer.Users;
+
 /// <summary>
 /// Returns basic user info according to their identifiers.
 /// Possible errors
@@ -14,34 +15,67 @@ namespace MyTelegram.Messenger.Handlers.LatestLayer.Users;
 /// <remarks>
 /// Access: [User ✔] [Bot ✔] [Anonymous ✖]
 /// </remarks>
-internal sealed class GetUsersHandler(IUserConverterService userConverterService) : RpcResultObjectHandler<MyTelegram.Schema.Users.RequestGetUsers, TVector<MyTelegram.Schema.IUser>>
+internal sealed class GetUsersHandler(IAccessHashHelper accessHashHelper, IUserConverterService userConverterService)
+    : RpcResultObjectHandler<MyTelegram.Schema.Users.RequestGetUsers, TVector<MyTelegram.Schema.IUser>>
 {
-    protected override async Task<TVector<IUser>> HandleCoreAsync(IRequestInput input, MyTelegram.Schema.Users.RequestGetUsers obj)
+    protected override async Task<TVector<IUser>> HandleCoreAsync(IRequestInput input,
+        MyTelegram.Schema.Users.RequestGetUsers obj)
     {
         var userIds = new List<long>();
+        var result = new TVector<IUser>();
+
         foreach (var inputUser in obj.Id)
         {
-            //await _accessHashHelper.CheckAccessHashAsync(inputUser);
             switch (inputUser)
             {
                 case TInputUser inputUser1:
-                    userIds.Add(inputUser1.UserId);
+                    if (await accessHashHelper.IsAccessHashValidAsync(input, inputUser1.UserId, inputUser1.AccessHash))
+                    {
+                        userIds.Add(inputUser1.UserId);
+                    }
+
+                    result.Add(new TUserEmpty
+                    {
+                        Id = inputUser1.UserId
+                    });
                     break;
                 case TInputUserEmpty:
                     userIds.Add(input.UserId);
+                    result.Add(new TUserEmpty
+                    {
+                        Id = input.UserId
+                    });
                     break;
-                case TInputUserFromMessage inputUserFromMessage:
-                    userIds.Add(inputUserFromMessage.UserId);
+                case TInputUserFromMessage:
+                    result.Add(new TUserEmpty
+                    {
+                        Id = 0
+                    });
+                    //userIds.Add(inputUserFromMessage.UserId);
                     break;
                 case TInputUserSelf:
                     userIds.Add(input.UserId);
+                    result.Add(new TUserEmpty
+                    {
+                        Id = input.UserId
+                    });
                     break;
                 default:
                     throw new NotImplementedException();
             }
         }
 
-        var users = await userConverterService.GetUserListAsync(input, userIds, false, false, input.Layer);
-        return[..users];
+        var users = (await userConverterService.GetUserListAsync(input, userIds, false, false, input.Layer))
+            .ToDictionary(k => k.Id);
+
+        for (int i = 0; i < result.Count; i++)
+        {
+            if (users.TryGetValue(result[i].Id, out var user))
+            {
+                result[i] = user;
+            }
+        }
+
+        return result;
     }
 }
