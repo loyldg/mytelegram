114597e4e386910e6d91f19a5b684eb8351e6155
mytelegram <mytelegramserver@outlook.com>
2026-01-13 16:05:15 +0800

    fix: GetChannelMembersByChannelIdQueryHandler

--- source/src/MyTelegram.QueryHandlers.MongoDB/Channel/GetChannelMembersByChannelIdQueryHandler.cs
+++ source/src/MyTelegram.QueryHandlers.MongoDB/Channel/GetChannelMembersByChannelIdQueryHandler.cs
@@ -2,17 +2,18 @@
 
 public class GetChannelMembersByChannelIdQueryHandler(IQueryOnlyReadModelStore<ChannelMemberReadModel> store) :
     IQueryHandler<GetChannelMembersByChannelIdQuery,
-    IReadOnlyCollection<IChannelMemberReadModel>>
+        IReadOnlyCollection<IChannelMemberReadModel>>
 {
     public async Task<IReadOnlyCollection<IChannelMemberReadModel>> ExecuteQueryAsync(
         GetChannelMembersByChannelIdQuery query,
         CancellationToken cancellationToken)
     {
-        Expression<Func<ChannelMemberReadModel, bool>> filter = p => !p.Left && !p.Kicked && p.ChannelId == query.ChannelId;
+        Expression<Func<ChannelMemberReadModel, bool>> filter = p => /*!p.Left && !p.Kicked && */p.ChannelId == query.ChannelId;
         filter = filter.WhereIf(query.MemberUserIdList.Count > 0, p => query.MemberUserIdList.Contains(p.UserId))
                 .WhereIf(query.OnlyAdmin, p => p.IsAdmin)
                 .WhereIf(query.OnlyBots, p => p.IsBot)
                 .WhereIf(query.OnlyKicked, p => p.Kicked)
+                .WhereIf(!query.OnlyKicked, p => !p.Left && !p.Kicked)
                 .WhereIf(query.OnlyBanned, p => p.BannedRights != 0)
             ;
 
