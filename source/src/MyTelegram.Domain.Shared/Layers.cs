c9b420c6b8dfa7dadf66590642cccb7348443f69
mytelegram <mytelegramserver@outlook.com>
2026-04-15 23:08:05 +0800

    refactor: Remove unused layers

--- source/src/MyTelegram.Domain.Shared/Layers.cs
+++ source/src/MyTelegram.Domain.Shared/Layers.cs
@@ -2,52 +2,7 @@
 
 public class Layers
 {
-    public const int Layer143 = 143;
-    public const int Layer144 = 144;
-    public const int Layer145 = 145;
-    public const int Layer146 = 146;
-    public const int Layer147 = 147;
-    public const int Layer148 = 148;
-    public const int Layer149 = 149;
-    public const int Layer150 = 150;
-    public const int Layer151 = 151;
-    public const int Layer152 = 152;
-    public const int Layer153 = 153;
-    public const int Layer154 = 154;
-    public const int Layer155 = 155;
-    public const int Layer156 = 156;
-    public const int Layer158 = 158;
-    public const int Layer160 = 160;
-    public const int Layer164 = 164;
-    public const int Layer165 = 165;
-    public const int Layer166 = 166;
-    public const int Layer167 = 167;
-    public const int Layer170 = 170;
-    public const int Layer171 = 171;
-    public const int Layer172 = 172;
-    public const int Layer173 = 173;
-    public const int Layer174 = 174;
-    public const int Layer177 = 177;
-    public const int Layer179 = 179;
-    public const int Layer181 = 181;
-    public const int Layer183 = 183;
-    public const int Layer187 = 187;
-    public const int Layer190 = 190;
-    public const int Layer192 = 192;
-    public const int Layer195 = 195;
-    public const int Layer196 = 196;
-    public const int Layer197 = 197;
-    public const int Layer198 = 198;
-    public const int Layer200 = 200;
-    public const int Layer202 = 202;
-    public const int Layer204 = 204;
-    public const int Layer206 = 206;
-    public const int Layer211 = 211;
-    public const int Layer214 = 214;
-    public const int Layer216 = 216;
-    public const int Layer220 = 220;
-    public const int Layer221 = 221;
-    public const int Layer222 = 222;
+    public const int Layer224 = 224;
 
-    public const int LayerLatest = Layer222;
+    public const int LayerLatest = Layer224;
 }
