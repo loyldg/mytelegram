01719c140d401e33983b1c4a2acfcad182e0bb73
mytelegram <mytelegramserver@outlook.com>
2026-04-15 23:12:36 +0800

    fix: Adapt to Android using legacy API schema for forwarding messages

---
+++ source/src/MyTelegram.Messenger/Handlers/LayerN/Messages/ForwardMessagesHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace MyTelegram.Messenger.Handlers.LayerN.Messages;
+
+internal sealed class ForwardMessagesHandler(
+    IHandlerHelper handlerHelper,
+    IRequestConverter<MyTelegram.Schema.Messages.LayerN.RequestForwardMessages,
+        MyTelegram.Schema.Messages.RequestForwardMessages> dataConverter)
+    : ForwardRequestToNewHandler<
+        MyTelegram.Schema.Messages.LayerN.RequestForwardMessages,
+        MyTelegram.Schema.Messages.RequestForwardMessages
+    >(handlerHelper, dataConverter)
+{
+}
