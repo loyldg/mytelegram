8f4db5af786ceac6272cece5b9f9ffa87486092b
mytelegram <mytelegramserver@outlook.com>
2026-04-27 16:00:49 +0800

    chore: Create database indexes on command and query server startup

--- source/src/MyTelegram.Messenger.CommandServer/BackgroundServices/MyTelegramCommandServerBackgroundService.cs
+++ source/src/MyTelegram.Messenger.CommandServer/BackgroundServices/MyTelegramCommandServerBackgroundService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using MyTelegram.EventFlow.MongoDB;
 using MyTelegram.Messenger.Services.Caching;
 
 namespace MyTelegram.Messenger.CommandServer.BackgroundServices;
@@ -6,12 +7,14 @@ namespace MyTelegram.Messenger.CommandServer.BackgroundServices;
 public class MyTelegramCommandServerBackgroundService(
     ILogger<MyTelegramCommandServerBackgroundService> logger,
     IHandlerHelper handlerHelper,
+    IMongoDbIndexesCreator mongoDbIndexesCreator,
     IInMemoryCacheLoader inMemoryCacheLoader)
     : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Command server starting...");
+        await mongoDbIndexesCreator.CreateAllIndexesAsync();
         handlerHelper.InitAllHandlers();
         await inMemoryCacheLoader.LoadAsync();
 
--- source/src/MyTelegram.Messenger.QueryServer/BackgroundServices/MyTelegramQueryServerBackgroundService.cs
+++ source/src/MyTelegram.Messenger.QueryServer/BackgroundServices/MyTelegramQueryServerBackgroundService.cs
@@ -6,6 +6,7 @@ namespace MyTelegram.Messenger.QueryServer.BackgroundServices;
 public class MyTelegramQueryServerBackgroundService(
     ILogger<MyTelegramQueryServerBackgroundService> logger,
     //IHandlerHelper handlerHelper,
+    IMongoDbIndexesCreator mongoDbIndexesCreator,
     IInMemoryCacheLoader inMemoryCacheLoader,
     ILanguageCacheService languageCacheService)
     : BackgroundService
@@ -13,6 +14,7 @@ public class MyTelegramQueryServerBackgroundService(
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Query server starting...");
+        await mongoDbIndexesCreator.CreateAllIndexesAsync();
         await inMemoryCacheLoader.LoadAsync();
         await languageCacheService.LoadAllLanguagesAsync();
         await languageCacheService.LoadAllLanguageTextAsync();
